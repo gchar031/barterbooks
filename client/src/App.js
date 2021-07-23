098e1cc469e27666fba396d4e7b980ad1b5154f7
gchar031 <43050919+gchar031@users.noreply.github.com>
2021-07-23 09:41:23 -0400

    app js complete and layout code complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom'
-import Home from './screens/Home'
+import { Route, Switch, useHistory } from 'react-router-dom'
 import Register from './screens/Register'
 import Login from './screens/Login'
+import Layout from './components/Layout';
 import MainContainer from './containers/MainContainer'
 import { registerStudent, loginStudent, verifyStudent, removeToken } from './services/auth'
 
 function App() {
   const [currentStudent, setCurrentS] = useState(null)
+  const history = useHistory()
 
   useEffect(() => {
     const handleVerify = async () => {
@@ -17,13 +18,38 @@ function App() {
     }
     handleVerify()
   }, [])
+
+  const handleLogin = async (formData) => {
+    const studentData = await loginStudent(formData)
+    setCurrentS(studentData)
+    history.push('/')
+  }
+
+  const handleRegister = async (formData) => {
+    const studentData = await registerStudent(formData);
+    setCurrentS(studentData);
+    history.push("/");
+  };
+
+  const handleLogout = async () => {
+    setCurrentS(null)
+    localStorage.removeItem('authToken')
+    removeToken()
+    history.push('/')
+  }
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-      </Switch>
+      <Layout currentStudent={currentStudent} handleLogout={handleLogout}>
+        <Switch>
+          <Route path="/" component={MainContainer} />
+          <Route path="/register">
+            <Register handleRegister={handleRegister} />
+            </Route>
+          <Route path="/login">
+            <Login handleLogin={handleLogin} />
+            </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout(props) {
+  const { currentUser, handleLogout } = props;
+  return (
+    <div>
+      <Header currentUser={currentUser} handleLogout={handleLogout} />
+      <hr />
+      {props.children}
+      <hr />
+      <Footer />
+    </div>
+  );
+}
