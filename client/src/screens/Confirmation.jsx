f88c442e95de9a27efcaba101287b543101c2fee
gchar031 <43050919+gchar031@users.noreply.github.com>
2021-07-24 23:53:30 -0400

    content for confirmation and home page added

--- client/src/screens/Confirmation.jsx
+++ client/src/screens/Confirmation.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function Confirmation() {
   return (
-    <div>
+    <div id="confirmReq">
       <h2 className="subtitles">Confirmation</h2>
+      <section id="confirmBody">
+        <p>
+          Your exchange request has been confirmed! To contact the barterer for
+          approval or more information <u id="confirmLink">click here</u>.
+        </p>
+      </section>
     </div>
   );
 }
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>
+    <div className="about">
       <h2 className="subtitles">About</h2>
+      <p>
+        College supllies can be very expensive and add up over time especially
+        when it comes to books. If you need a book for an upcoming course view
+        all the barters we have by clicking the button below!
+      </p>
+      <Link to="/books">View Barters for Books</Link>
     </div>
   );
 }
