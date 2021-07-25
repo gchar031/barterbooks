2d4b518f43cac861227e60d5564c702a36b13962
gchar031 <43050919+gchar031@users.noreply.github.com>
2021-07-25 14:05:30 -0400

    font awsome kit added and footer content added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import '../styles/Footer.css'
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer>
+      <p id="developer">By Gilda Charles</p>
+      <div id="socialIcons">
+      <a href="linkedin.com/in/gilda-char">
+        <i class="fab fa-github"></i>
+      </a>
+      <a href="https://github.com/gchar031">
+        <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </footer>
+  );
 }
