7c181ca954c0f7078a3183ba9f9a65aa762cf1ba
gchar031 <43050919+gchar031@users.noreply.github.com>
2021-07-25 23:04:47 -0400

    logout in header designed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,25 +5,25 @@ import { useState } from 'react';
 export default function Header(props) {
   const { currentStudent, handleLogout } = props;
   const [menuDisplay, setMenuDisplay] = useState(null)
-
+  
   function checkLogin() {
     return currentStudent ? (
-      <div id="welcome-user">
-        <h3> Hello, {currentStudent.username} </h3>
-        <button onClick={handleLogout}>Logout</button>
+      <div id="welcome" onClick={handleLogout}>
+        <i className="fas fa-sign-out-alt logout"></i>
+        <p className="menup">Logout</p>
       </div>
     ) : (
       <div className="login-btns">
         <div id="register-btn">
           <Link to="/register">
-            <i class="fas fa-user-plus">
+            <i className="fas fa-user-plus">
               <p className="menup">Register</p>
             </i>
           </Link>
         </div>
         <div id="login-btn">
           <Link to="/login" className="menu-link">
-            <i class="fas fa-sign-in-alt"></i>
+            <i className="fas fa-sign-in-alt"></i>
             <p className="menup">Login</p>
           </Link>
         </div>
@@ -43,7 +43,7 @@ export default function Header(props) {
       <Link to="/" className="title-link">
         <h1 id="title">Barter Books</h1>
       </Link>
-      <div className="login-register">{checkLogin()}</div>
+      {checkLogin()}
       <ul className={"menu " + menuDisplay}>
         <span id="close-menu" onClick={() => setMenuDisplay("close")}>
           Close
